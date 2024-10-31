581a355d50e5f0291e515695c45ce6369ee0b74b
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2024-10-30 21:48:32 -0600

    pruebas con departamento

--- cypress/e2e/test2.cy.ts
+++ cypress/e2e/test2.cy.ts
@@ -7,9 +7,27 @@ describe('Documentos y trámites', function () {
         cy.visit('https://www.munisc.go.cr/Paginas/Visitantes/Documentos.aspx');
     });
 
-    it('PR031- Iniciar Sesión', function () { // Prueba con usuario valido
+    afterEach(function () {
+        cy.clearCookies();
+        cy.clearLocalStorage();
+    });
 
-        
+    it('Decargar el primer documento del departamento Acueducto', function () {
+
+        cy.xpath('/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[1]/label[2]/input').click();
+
+        cy.xpath('/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[2]/select').select('Acueducto');
+
+        cy.get('table tbody tr').eq(1).find('td').eq(2).find('img').click();
+    })
+
+    it('Notificar al usuario cuando no hay documentos disponibles', function () {
+
+        cy.xpath('/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[1]/label[2]/input').click();
+
+        cy.xpath('/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[2]/select').select('Administración');
+
+        cy.xpath('/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[2]/fieldset/div[4]/div/table/tbody/tr/td/div/span').should('have.text', 'No existen documentos relacionados.');
     })
 
 });
